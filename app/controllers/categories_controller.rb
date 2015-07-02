a27fa5aff60de205fdb5138a01c2936a556b2246
David Garber <davidgarber@gmail.com>
2015-07-02 08:26:03 -0700

    Edit a category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,20 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
   private
   def category_params
     params.require(:category).permit(:name, :description)
