a82158f8362f71ef5496f76c432522a3365b590d
David Garber <davidgarber@gmail.com>
2015-07-02 07:52:04 -0700

    Validate the presence of a description.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   validates :name, :presence => true
+  validates :description, :presence => true
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Category do
   it {should validate_presence_of :name}
+  it {should validate_presence_of :description}
+
 end
