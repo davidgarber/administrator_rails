a3799328654a23d5c4914030f2581c43aa4deb61
David Garber <davidgarber@gmail.com>
2015-07-10 09:54:02 -0700

    Describe the edit a post process. SimpleCov 96.87%

---
+++ spec/features/edit_post_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe "edit a post" do
+  it "edits a post" do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    post = FactoryGirl.create(:post)
+    visit post_path(post)
+    click_on 'Edit'
+    fill_in 'Title', :with => "Blog"
+    fill_in 'Body', :with => "This is not a blog."
+    click_button 'Update Post'
+    expect(page).to have_content 'Blog'
+  end
+end
