371c1f4c2d9eb168d74627e6af02d5936d52422c
David Garber <davidgarber@gmail.com>
2015-07-10 09:57:08 -0700

    Returns an error when title or body are blank. SimpleCov 97.26%

--- spec/features/edit_post_pages_spec.rb
+++ spec/features/edit_post_pages_spec.rb
@@ -15,4 +15,16 @@ describe "edit a post" do
     click_button 'Update Post'
     expect(page).to have_content 'Blog'
   end
+
+  it "returns error when body or title are blank" do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    post = FactoryGirl.create(:post)
+    visit post_path(post)
+    click_on 'Edit'
+    fill_in 'Title', :with => ""
+    fill_in 'Body', :with => ""
+    click_button 'Update Post'
+    expect(page).to have_content 'errors'
+  end
 end
